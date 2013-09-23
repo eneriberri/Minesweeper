404b32b9ad3420525f5febcf71cf8ae608073eeb
App Academy Student <student@appacademy.io>
2013-09-23 15:13:15 -0700

    Fill actual_board

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,26 +1,105 @@
 class Board
   def initialize(size = 9)
-    @display_board = [["*"] * size] * size
-    @actual_board = [[nil] * size] * size
+#    @display_board = [["*"] * size] * size
+    @size = size
+    @display_board = (0...size).map { [Tile.new(self)] * size }
+    @actual_board = (0...size).map { [Tile.new(self)] * size }
     @mines = ((size == 9) ? 10 : 40)
+    generate_bombs
+#    complete_actual_board
+    display_actual_board
 
     size.times do |row|
       size.times do |col|
-        @board[row][col] = Tile.new(self)
+        @display_board[row][col] = Tile.new(self)
       end
     end
   end
 
-  def generate_board
-    board =
+  def generate_bombs
+    i = 0
+    until i == @mines
+      row = rand(@size)
+      col = rand(@size)
+      if @actual_board[row][col].value == "*"
+        @actual_board[row][col] = Tile.new(self, "b")
+        i += 1
+      end
+    end
   end
+
+  def generate_bombs_test
+    @actual_board[3][3] = Tile.new(self, "b")
+  end
+
+  #returns all of a tile's neighbors
+  #determine bombs surrounding the tile
+  def show_bombs(tile_loc)
+    neighbors = []
+    possible_neighbors = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
+
+
+    possible_neighbors.each do |neighbor|
+      possible_neighbor = [neighbor.first + tile_loc.first, neighbor.last + tile_loc.last]
+      neighbors << possible_neighbor if valid?(possible_neighbor)
+    end
+
+    @actual_board[tile_loc.first][tile_loc.last] = Tile.new(self, count_bombs(neighbors))
+  end
+
+  def valid?(coords)
+    coords.none? { |coord| coord < 0 || coord >= @size }
+  end
+
+  #return the number of bombs that tile is surrounded by
+  def count_bombs(neighbors)
+    num = 0
+    neighbors.each do |neighbor|
+      num += 1 if @actual_board[neighbor.first][neighbor.last].is_bomb?
+    end
+    num
+  end
+
+  def complete_actual_board
+
+    @actual_board.each_index do |row|
+      @actual_board.each_index do |col|
+        tile = @actual_board[row][col]
+        next if tile.is_bomb?
+
+        tile.value = show_bombs([row,col])
+      end
+    end
+  end
+
+  def display_actual_board
+    @actual_board.each_index do |row|
+      @actual_board.each_index do |col|
+        print "#{@actual_board[row][col].value} "
+
+      end
+      puts ""
+    end
+  end
+
 end
 
 class Tile
-  def initialize(board)
-    board
+  attr_accessor :value
+  def initialize(board, value = "*")
+    @board = board
+    @value = value
   end
+
+  def is_bomb?
+    @value == "b"
+  end
+
 end
+
 
 class Minesweeper
 end
+
+
+board = Board.new
